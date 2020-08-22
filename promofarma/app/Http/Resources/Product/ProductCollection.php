c10e4d3e4229eb7e11217d6911d9d96888cd12eb
alejandro <alejandro.tadeo.romero@gmail.com>
2020-08-22 12:37:52 +0200

    some et api created, allproducts, productByID, allReviews

--- promofarma/app/Http/Resources/Product/ProductCollection.php
+++ promofarma/app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ProductCollection extends ResourceCollection
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +16,21 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'price' => $this->price,
+            'discount' => $this->discount,
+            'rating' => $this->averageRating(),
+            'href' => [
+                'link' => route('products.show', $this->id)
+            ]
+        ];
+    }
+
+    private function averageRating()
+    {
+        return $this->reviews->count() > 0 ?
+            round($this->reviews->sum('star') / $this->reviews->count(), 2) :
+            'Be first to review it!';
     }
 }
