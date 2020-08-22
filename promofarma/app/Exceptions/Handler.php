460e8480b6cf9b5eeed6c46f832220321251c1c7
alejandro <alejandro.tadeo.romero@gmail.com>
2020-08-22 15:09:04 +0200

    handle exception badly

--- promofarma/app/Exceptions/Handler.php
+++ promofarma/app/Exceptions/Handler.php
@@ -2,8 +2,11 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
+use Symfony\Component\HttpFoundation\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +53,21 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($request->expectsJson()) {
+            if ($exception instanceof ModelNotFoundException) {
+                return response()->json([
+                    'errors' => 'Product Model not found'
+
+                ], Response::HTTP_NOT_FOUND);
+            }
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'errors' => 'Incorrect route'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
         return parent::render($request, $exception);
     }
 }
